ca2e4fd150d02acf27ed12fdb0b2c2ea5a6aefcb
mago_d_Mercurio <davila.feria@gmail.com>
2023-12-08 22:27:45 -0600

    create 03-typing.ts 03-demo.ts

---
+++ src/03-demo.ts
@@ -0,0 +1,2 @@
+let myProductName = 'product 1';
+let myProductPrice = 123;
---
+++ src/03-typing.ts
@@ -0,0 +1,13 @@
+(() => {
+  let myProductName = 'product 1';
+  let myProductPrice = 123;
+  
+  myProductName = 'change';
+  myProductName.toLowerCase();
+  
+  myProductPrice.toFixed();
+  
+  const myProductStock = 1000;
+  const myProductName2 = 'product 2'
+})();
+
