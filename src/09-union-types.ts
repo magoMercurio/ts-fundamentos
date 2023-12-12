3509cabb1b0941408de182c166b62eb2abc7158b
mago_d_Mercurio <davila.feria@gmail.com>
2023-12-11 23:24:52 -0600

    Update 09-union-types.ts

--- src/09-union-types.ts
+++ src/09-union-types.ts
@@ -1,3 +1,15 @@
 (() => {
-  let myDynamicVar: 
+  let userId: string | number;
+  userId = 1212
+  userId = 'asdasd'
+
+  function greeting(myText: string | number) {
+    if (typeof myText === 'string') {
+      console.log(`string ${myText.toLowerCase}`)
+    } else {
+      console.log(`numbeer ${myText.toFixed(1)}`)
+    }
+  }
+  greeting('Hola')
+  greeting(1212)
 })()
